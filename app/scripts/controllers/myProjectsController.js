dede4de20852da06ed67fa41b03000b366675b8c
Pawel Kromplewski <pawel.kromplewski@pagepro.pl>
2015-09-10 15:16:53 +0200

    my project merge view with controller

--- app/scripts/controllers/myProjectsController.js
+++ app/scripts/controllers/myProjectsController.js
@@ -1,6 +1,24 @@
 (function() {
   'use strict';
-  var MyProjectsController = function($scope) {};
+  var MyProjectsController = function($scope) {
+
+    $scope.myProjects = [ // temp solution
+        {
+            id: 1, // used for proper redirecting to details view
+            updated: '22.02.2015', // there will be timestamp probably, pure date for now
+            progress: 33, // in percentage. We have to decide if we will get this number from API or we will calculate it on our side
+            name: 'Temporibus autem autem autem autem',
+            templatesCount: 29
+        }, // all names of variables could be different
+        {
+            id: 2,
+            updated: '19.09.2015',
+            progress: 99,
+            name: 'Frontend App',
+            templatesCount: 3
+        }
+    ];
+  };
 
   MyProjectsController.$inject = ['$scope'];
   angular.module('frontendApp').controller('MyProjectsController', MyProjectsController);
