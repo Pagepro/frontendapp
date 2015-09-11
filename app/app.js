24b14ee57c565208c74c70579d5a7f02e26fb91b
Pawel Kromplewski <pawel.kromplewski@pagepro.pl>
2015-09-11 11:52:10 +0200

    add minification names to app js

--- app/app.js
+++ app/app.js
@@ -8,56 +8,61 @@
 
 
   frontendApp
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-    $locationProvider.html5Mode(true);
-    $locationProvider.hashPrefix('!');
-    $urlRouterProvider.otherwise('/auth/login');
-    $stateProvider
-      .state('accountState', {
-        url: '/auth',
-        templateUrl: 'partials/templates/auth/account.html',
-        controller: 'AccountController',
-        controllerAs: 'AC',
-        pageName: 'Account'
-      })
-      .state('accountState.login', {
-        url: '/login',
-        templateUrl: 'partials/templates/auth/login.html',
-        controller: 'LoginController',
-        controllerAs: 'LC',
-        pageName: 'Login'
-      })
-      .state('accountState.register', {
-        url: '/register',
-        templateUrl: 'partials/templates/auth/registration.html',
-        controller: 'RegistrationController',
-        controllerAs: 'RC',
-        pageName: 'Register'
-      })
-      // logged in
-      .state('myProjectsState', {
-        url: '/myProjects',
-        templateUrl: 'partials/templates/panel/myProjects.html',
-        controller: 'MyProjectsController',
-        controllerAs: 'MPC',
-        pageName: 'My Projects'
+  .config(['$stateProvider',
+           '$urlRouterProvider',
+           '$locationProvider',
+    function ($stateProvider, $urlRouterProvider, $locationProvider) {
+      $locationProvider.html5Mode(true);
+      $locationProvider.hashPrefix('!');
+      $urlRouterProvider.otherwise('/auth/login');
+      $stateProvider
+        .state('accountState', {
+          url: '/auth',
+          templateUrl: 'partials/templates/auth/account.html',
+          controller: 'AccountController',
+          controllerAs: 'AC',
+          pageName: 'Account'
+        })
+        .state('accountState.login', {
+          url: '/login',
+          templateUrl: 'partials/templates/auth/login.html',
+          controller: 'LoginController',
+          controllerAs: 'LC',
+          pageName: 'Login'
+        })
+        .state('accountState.register', {
+          url: '/register',
+          templateUrl: 'partials/templates/auth/registration.html',
+          controller: 'RegistrationController',
+          controllerAs: 'RC',
+          pageName: 'Register'
+        })
+        // logged in
+        .state('myProjectsState', {
+          url: '/myProjects',
+          templateUrl: 'partials/templates/panel/myProjects.html',
+          controller: 'MyProjectsController',
+          controllerAs: 'MPC',
+          pageName: 'My Projects'
+        });
+  }])
+  .run(['$state',
+        '$rootScope',
+    function ($state, $rootScope) {
+      // todo
+      // here's a place to check whether a user is logged in
+      // if so, redirect him to his projects,
+      // else, redirect to login
+      if (true) {
+        // $state.go('accountState.login');
+      } else {
+        $state.go('homeState');
+      }
+      $rootScope.$on('$stateChangeSuccess', function (event, data) {
+        $rootScope.pageName = data.pageName;
+        $rootScope.showBodyBackground = (data.name.indexOf('accountState') > -1);
       });
-  })
-  .run(function ($state, $rootScope) {
-    // todo
-    // here's a place to check whether a user is logged in
-    // if so, redirect him to his projects,
-    // else, redirect to login
-    if (true) {
-      // $state.go('accountState.login');
-    } else {
-      $state.go('homeState');
-    }
-    $rootScope.$on('$stateChangeSuccess', function (event, data) {
-      $rootScope.pageName = data.pageName;
-      $rootScope.showBodyBackground = (data.name.indexOf('accountState') > -1);
-    });
-    $rootScope.communicator = {};
-  });
+      $rootScope.communicator = {};
+  }]);
 
 }());
