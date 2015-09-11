39caa648f3d84be0d05c4cc04977e0c6b93b2150
Pawel Kromplewski <pawel.kromplewski@pagepro.pl>
2015-09-11 11:20:54 +0200

    add missing gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,7 +5,6 @@ var browser = require('browser-sync');
 var reload = browser.reload;
 var rev = require('gulp-rev');
 var autoprefixer = require('gulp-autoprefixer');
-var htmllint = require('gulp-htmllint');
 var eslint = require('gulp-eslint');
 var plumber = require('gulp-plumber');
 var phantomjssmith = require('gulp.spritesmith');
@@ -27,18 +26,11 @@ gulp.task('serve', ['sass'], function() {
     });
 });
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**", ['sass']);
-    gulp.watch("src/img/**", ['copy']);
+    gulp.watch("partials/sass/**", ['sass']);
     gulp.watch(['app/templates/**/*.html', 'src/**/*.js'], reload);
 });
-gulp.task('copy', function() {
-    gulp.src('src/img/**')
-      .pipe(gulp.dest('app/img'));
-    gulp.src('src/fonts/**')
-      .pipe(gulp.dest('app/fonts'));
-});
 gulp.task('sass', function () {
-    gulp.src('./src/sass/**/*.scss')
+    gulp.src('./partials/sass/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(sourcemaps.write())
@@ -46,15 +38,6 @@ gulp.task('sass', function () {
         .pipe(reload({
             stream: true
         }));
-});
-gulp.task('sprites', function () {
-    var spriteData = gulp.src('src/img/sprite/*.png').pipe(spritesmith({
-        imgName: 'app/img/sprite.png',
-        cssName: 'src/sass/sprites.scss',
-        imgPath: '../app/img/sprite.png',
-        engine: phantomjssmith
-    }));
-    return spriteData.pipe(gulp.dest('.'));
 });
 gulp.task('autoprefixer', function () {
     return gulp.src('app/css/*.css')
@@ -64,11 +47,6 @@ gulp.task('autoprefixer', function () {
         }))
         .pipe(gulp.dest('app/css/'));
 });
-gulp.task('htmllint', function() {
-    return gulp.src('index.html')
-        .pipe(htmllint());
-});
 
 gulp.task('default', ['sass', 'copy', 'watch', 'serve']);
 gulp.task('compile', ['sass']);
-gulp.task('publish', ['rev', 'autoprefixer', 'htmllint']);
