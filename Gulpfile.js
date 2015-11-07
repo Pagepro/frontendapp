9a7efe968fe236e08e3535cb99a1b667fd08c84d
Kuba Wiszniewski <kuba.wiszniewski@pagepro.pl>
2015-11-07 13:02:53 +0100

    added new build system and dependency injection, based on gulp concat now

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,47 +6,66 @@ var reload = browser.reload;
 var rev = require('gulp-rev');
 var autoprefixer = require('gulp-autoprefixer');
 var eslint = require('gulp-eslint');
-var plumber = require('gulp-plumber');
-var phantomjssmith = require('gulp.spritesmith');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('rev', function () {
-    gulp.src('*.html')
-        .pipe(rev())
-        .pipe(gulp.dest('.'));
+
+
+gulp.task('rev', function() {
+  gulp.src('*.html')
+    .pipe(rev())
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('js', function() {
+  gulp.src(['app/**/module.js', 'app/**/*.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('out'))
 });
 
 gulp.task('serve', ['sass'], function() {
-    browser({
-        port: process.env.PORT || 4500,
-        open: false,
-        ghostMode: false,
-        server: {
-            baseDir: '.'
-        }
-    });
+  browser({
+    port: process.env.PORT || 4500,
+    open: false,
+    ghostMode: false,
+    server: {
+      baseDir: '.'
+    }
+  });
 });
-gulp.task('watch', function() {
-    gulp.watch('partials/sass/**', ['sass']);
-    gulp.watch(['partials/templates/**/*.html', 'src/**/*.js'], reload);
+
+gulp.task('watch', ['js'], function() {
+  gulp.watch('partials/sass/**', ['sass']);
+  gulp.watch(['partials/templates/**/*.html', 'app/**/*.html'], reload);
+  gulp.watch(['app/**/*.js'], ['js']);
 });
-gulp.task('sass', function () {
-    gulp.src('./partials/sass/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./partials/css'))
-        .pipe(reload({
-            stream: true
-        }));
+
+gulp.task('prod', function() {
+  gulp.src(['out/app.js'])
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('out'))
 });
-gulp.task('autoprefixer', function () {
-    return gulp.src('partials/css/*.css')
-        .pipe(autoprefixer({
-            browsers: ['> 1%'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('partials/css/'));
+
+gulp.task('sass', function() {
+  gulp.src('./partials/sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./partials/css'))
+    .pipe(reload({
+      stream: true
+    }));
+});
+gulp.task('autoprefixer', function() {
+  return gulp.src('partials/css/*.css')
+    .pipe(autoprefixer({
+      browsers: ['> 1%'],
+      cascade: false
+    }))
+    .pipe(gulp.dest('partials/css/'));
 });
 
 gulp.task('default', ['sass', 'watch', 'serve']);
-gulp.task('compile', ['sass']);
+gulp.task('compile', ['sass', 'prod']);
