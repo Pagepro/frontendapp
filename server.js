a6f4c1686a4ade50df1e4074f08d176865a1c293
Kuba Wiszniewski <kuba.wiszniewski@pagepro.pl>
2015-11-07 14:46:19 +0100

    replaced static fake server data with dynamically generated, added percentage to project pages, back to projects link in a specific project works fine now

--- server.js
+++ server.js
@@ -1,87 +1,18 @@
 var express = require('express');
 var cors = require('cors');
-var Chance = require('chance');
-var chance = new Chance();
+
 var fs = require('fs');
-var projects;
 
-fs.readFile( __dirname + '/serverData.js', function (err, data) {
-  if (err) {
-    throw err;
-  }
-  tt = data;
-});
-
-fs.readFile( __dirname + '/test.json', function (err, data) {
-  if (err) {
-    throw err;
-  }
-  /**
-   * Generate the array using
-   * http://beta.json-generator.com/
-
-   * Update this markup every time!
-[
-  {
-    'repeat:7': {
-      _id: '{{objectId()}}',
-      id: '{{index()}}',
-      statusCode: '{{integer(0, 3)}}',
-      projectProgress: '{{integer(0, 100)}}',
-      dateUpdated: '{{moment(this.date(new Date(2015, 2, 1), new Date())).valueOf()}}',
-      dateAdded: '{{moment(this.date(new Date(2014, 0, 1), new Date())).valueOf()}}',
-      dateFinished: '{{moment(this.date(new Date(2014, 0, 1), new Date())).valueOf()}}',
-      templates: [
-        {
-          'repeat:5': {
-            statusCode: '{{integer(0, 3)}}',
-            id: '{{index()}}',
-            image: 'https://placeimg.com/640/480/any?v={{integer(4, 100)}}',
-            name: function () {
-              return 'templateName' + '{{index()}}';
-            }
-          }
-        }
-      ],
-      files: [
-        {
-          'repeat:3': {
-            id: '{{index()}}',
-            extension: '{{random("zip", "psd", "7z")}}',
-            size: '{{integer(20, 1024)}}',
-            dateUpdated: '{{moment(this.date(new Date(2015, 2, 1), new Date())).valueOf()}}',
-            name: function () {
-              return 'templateName' + '{{index()}}';
-            }
-          }
-        }
-      ],
-      fileName: function (tags) {
-        var names = ['RZF', 'KRD', 'SOK'];
-        return names[tags.integer(0, names.length - 1)] + '_{{surname()}}';
-      }
-    }
-  }
-]
-  */
-  projects = JSON.parse(data);
-});
+var projects = require('./serverData');
 
 var app = express();
-app.use(cors())
+
+app.use(cors());
 
 app.use(express.static(__dirname + '/'));
 
 app.get('/projects/:id', function(req, res) {
-  var customerId = parseInt(req.params.id);
-  var data = {};
-  for (var i = 0, len = projects.length; i < len; i++) {
-    if (projects[i].id === customerId) {
-      data = projects[i];
-      break;
-    }
-  }
-  res.json(data);
+  res.json(projects[req.params.id]);
 });
 
 app.get('/projects', function(req, res) {
--- serverData.js
+++ serverData.js
@@ -4,32 +4,55 @@ var _ = require('lodash');
 var Chance = require('chance');
 var chance = new Chance();
 
-var projects = function() {
-  var templates = ['o'];
-  var files = ['b'];
-  var results;
-  chance.mixin({
-    'projects': function() {
-      return {
-        _id: _.uniqueId(),
-        id: chance.random(0, 100),
-        projectProgress: chance.first(10, 100),
-        dateUpdated: chance.birthday(),
-        dateAdded: chance.birthday(),
-        dateFinished: chance.birthday(),
-        templates: templates,
-        files: files,
-        fileName: chance.name()
-      };
-    }
-  });
+var projects = [];
 
-  while (results.length < _.random(1, 40)) {
-    results.push(chance.projects());
+function Templates() {
+  var templates = [];
+  while (templates.length < _.random(4, 30)) {
+    templates.push({
+      "statusCode": _.random(0, 4),
+      "id": _.uniqueId(),
+      "image": "https://placeimg.com/640/480/any?v=" + _.uniqueId(),
+      "name": chance.name({
+        middle: true
+      })
+    });
   }
-  return results;
-};
+  return templates;
+}
 
-return {
-  projects: projects
-};
+function Files() {
+  var files = [];
+  while (files.length < _.random(1, 30)) {
+    files.push({
+      "id": _.uniqueId(),
+      "extension": function() {
+        var extensions = ['zip', 'psd', 'jpg']
+        return extensions[_.random(0, 2)];
+      },
+      "size": _.random(10, 400),
+      "dateUpdated": chance.birthday(),
+      "name": chance.name()
+    });
+  }
+  return files;
+}
+
+while (projects.length < _.random(10, 40)) {
+  projects.push({
+    _id: chance.guid(),
+    id: _.uniqueId(),
+    dateUpdated: chance.birthday(),
+    dateAdded: chance.birthday(),
+    dateFinished: chance.birthday(),
+    fileName: chance.name(),
+    progress: _.random(0, 100)
+  });
+}
+
+_.forEach(projects, function(result) {
+  result.templates = new Templates();
+  result.files = new Files();
+});
+
+module.exports = projects;
