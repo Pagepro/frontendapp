7e783ab5bd739c7a82ee7fe5fe3c832a065a5673
Kuba Wiszniewski <kuba.wiszniewski@pagepro.pl>
2016-07-27 09:57:33 +0200

    updating tickets status updates filtered list now, also displaying all available statuses in select since you cant select the already selected one anyways

--- app/panel/directives/inlineToggler/inlineToggler.js
+++ app/panel/directives/inlineToggler/inlineToggler.js
@@ -1,11 +1,11 @@
 (function() {
   'use strict';
 
-  var inlineToggler = function(ticketsService, ticketStatusService, toaster) {
+  var inlineToggler = function(ticketsService, ticketStatusService, toaster, $timeout, spinnerService) {
     return {
       restrict: 'E',
+      scope: true,
       replace: true,
-      scope: false,
       templateUrl: 'app/panel/directives/inlineToggler/inlineToggler.html',
       link: function(scope) {
         /*
@@ -29,14 +29,24 @@
         });
 
         scope.setTicketStatus = function (newStatus) {
+          spinnerService.show('tickets-spinner');
           ticketsService.setTicketStatus(scope.projectId, scope.ticket.id, newStatus.code)
           .success(function (resp) {
+            // scope.tickets is just a refference?? need to go deep in scope to acheive actual $apply
+            scope.$parent.$parent.tickets = _.map(scope.$parent.$parent.tickets, function (ticket) {
+              return (ticket.id === scope.ticket.id) ? resp : ticket;
+            });
+
             scope.status = newStatus;
+            toaster.pop('success', 'Ticket\'s status successfully changed to ' + _.lowerCase(newStatus.labelContent));
           })
           .error(function() {
             toaster.pop('error', 'Couldn\'t update status.');
           })
-        }
+          .finally(function () {
+            spinnerService.hide('tickets-spinner');
+          });
+        };
 
         scope.toggleStatusList = function() {
           scope.statusListVisible = !scope.statusListVisible;
@@ -45,6 +55,6 @@
     };
   };
 
-  inlineToggler.$inject = ['ticketsService', 'ticketStatusService', 'toaster'];
+  inlineToggler.$inject = ['ticketsService', 'ticketStatusService', 'toaster', '$timeout', 'spinnerService'];
   angular.module('panelModule').directive('inlineToggler', inlineToggler);
 }());
