ba4345c43e4ec1fc919ab34898c9c8f57912df1e
Kuba Wiszniewski <kuba.wiszniewski@pagepro.pl>
2016-07-29 10:37:09 +0200

    clearing projects cache upon adding a new project

--- app/panel/services/projectsService.js
+++ app/panel/services/projectsService.js
@@ -1,25 +1,38 @@
 (function () {
   'use strict';
 
-  var projectsService = function ($http, $q, appSettings) {
+  var projectsService = function ($http, $q, appSettings, CacheFactory) {
+    CacheFactory('projectsCache', {
+      maxAge: 15 * 60 * 1000, // Items added to this cache expire after 15 minutes
+      cacheFlushInterval: 60 * 60 * 1000, // This cache will clear itself every hour
+      deleteOnExpire: 'aggressive' // Items will be deleted from this cache when they expire
+    });
     var baseUrl = appSettings.apiRoot + 'projects/';
+    var projectsCache = CacheFactory.get('projectsCache');
 
     this.getProjects = function (pageNo, projectStatus) {
       var requestUrl = baseUrl;
+      var params = '';
       var dfd = $q.defer();
 
       if (pageNo) {
-        requestUrl += ('?page=' + pageNo);
+        params += ('?page=' + pageNo);
       }
       if (projectStatus) {
-        requestUrl += ('?filter=' + projectStatus);
+        params += ('?filter=' + projectStatus);
       }
 
-      $http.get(requestUrl, {
-        cache: true
-      }).success(function (data) {
-        dfd.resolve(data);
-      });
+      requestUrl += params;
+
+      if (projectsCache.get(params)) {
+        dfd.resolve(projectsCache.get(params))
+      } else {
+        $http.get(requestUrl).success(function (data) {
+          projectsCache.put(params, data);
+          dfd.resolve(data);
+        });
+      }
+
 
       return dfd.promise;
     };
@@ -27,12 +40,13 @@
       return $http.get(baseUrl + projectId + '/');
     };
     this.createNewProject = function (projectName) {
+      projectsCache.removeAll();
       return $http.post(baseUrl, {
         name: projectName
       });
     };
   };
 
-  projectsService.$inject = ['$http', '$q', 'appSettings'];
+  projectsService.$inject = ['$http', '$q', 'appSettings', 'CacheFactory'];
   angular.module('panelModule').service('projectsService', projectsService);
 }());
